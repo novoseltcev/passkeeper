96ec356b590f8737f665b8c18929ecf04ade17e1
Новосельцев Станислав Андреевич <sanovoseltsev@domclick.ru>
2025-02-13 04:29:17 +0300

    chore: move domain's data to data.go

---
+++ internal/domains/secrets/data.go
@@ -0,0 +1,67 @@
+package secrets
+
+import (
+	"fmt"
+
+	"github.com/novoseltcev/passkeeper/internal/models"
+)
+
+type PasswordData struct {
+	Login    string
+	Password string
+	Meta     string
+}
+
+func (p PasswordData) ToString() string {
+	return fmt.Sprintf(`{"login":"%s","password":"%s","meta":"%s"}`, p.Login, p.Password, p.Meta)
+}
+
+func (p PasswordData) SecretType() models.SecretType {
+	return models.SecretTypePwd
+}
+
+type CardData struct {
+	Number string
+	Holder string
+	Exp    string
+	CVV    string
+	Meta   string
+}
+
+func (c CardData) ToString() string {
+	return fmt.Sprintf(
+		`{"number":"%s","holder":"%s","exp":"%s","cvv":"%s","meta":"%s"}`,
+		c.Number, c.Holder, c.Exp, c.CVV, c.Meta,
+	)
+}
+
+func (c CardData) SecretType() models.SecretType {
+	return models.SecretTypeCard
+}
+
+type TextData struct {
+	Content string
+	Meta    string
+}
+
+func (t TextData) ToString() string {
+	return fmt.Sprintf(`{"content":"%s","meta":"%s"}`, t.Content, t.Meta)
+}
+
+func (t TextData) SecretType() models.SecretType {
+	return models.SecretTypeTxt
+}
+
+type FileData struct {
+	Filename string
+	Content  string
+	Meta     string
+}
+
+func (f FileData) ToString() string {
+	return fmt.Sprintf(`{"filename":"%s","content":"%s","meta":"%s"}`, f.Filename, f.Content, f.Meta)
+}
+
+func (f FileData) SecretType() models.SecretType {
+	return models.SecretTypeFile
+}
--- internal/domains/secrets/service.go
+++ internal/domains/secrets/service.go
@@ -4,7 +4,6 @@ package secrets
 import (
 	"context"
 	"crypto/hmac"
-	"fmt"
 
 	"github.com/novoseltcev/passkeeper/internal/models"
 )
@@ -22,66 +21,6 @@ func NewPage[T any](items []T, pages uint64) *Page[T] {
 type ISecretData interface {
 	ToString() string
 	SecretType() models.SecretType
-}
-
-type PasswordData struct {
-	Login    string
-	Password string
-	Meta     string
-}
-
-func (p PasswordData) ToString() string {
-	return fmt.Sprintf(`{"login":"%s","password":"%s","meta":"%s"}`, p.Login, p.Password, p.Meta)
-}
-
-func (p PasswordData) SecretType() models.SecretType {
-	return models.SecretTypePwd
-}
-
-type CardData struct {
-	Number string
-	Holder string
-	Exp    string
-	CVV    string
-	Meta   string
-}
-
-func (c CardData) ToString() string {
-	return fmt.Sprintf(
-		`{"number":"%s","holder":"%s","exp":"%s","cvv":"%s","meta":"%s"}`,
-		c.Number, c.Holder, c.Exp, c.CVV, c.Meta,
-	)
-}
-
-func (c CardData) SecretType() models.SecretType {
-	return models.SecretTypeCard
-}
-
-type TextData struct {
-	Content string
-	Meta    string
-}
-
-func (t TextData) ToString() string {
-	return fmt.Sprintf(`{"content":"%s","meta":"%s"}`, t.Content, t.Meta)
-}
-
-func (t TextData) SecretType() models.SecretType {
-	return models.SecretTypeTxt
-}
-
-type FileData struct {
-	Filename string
-	Content  string
-	Meta     string
-}
-
-func (f FileData) ToString() string {
-	return fmt.Sprintf(`{"filename":"%s","content":"%s","meta":"%s"}`, f.Filename, f.Content, f.Meta)
-}
-
-func (f FileData) SecretType() models.SecretType {
-	return models.SecretTypeFile
 }
 
 // Service is a domain service for secrets.
